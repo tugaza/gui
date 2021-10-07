a56357f3dc2f7b4f7811b37c3b7a016536dc3525
pra <pra@mbarysas-t590>
2021-10-07 22:30:39 +0300

    httpd, omg

---
+++ httpd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=`dirname "${BASH_SOURCE[0]}"`
+if [[ -f $DIR/bootstrap.sh ]]
+then
+    . $DIR/bootstrap.sh
+else
+    echo "bootstrap not found"
+    exit 256
+fi
+
+bootstrap_load_module http/server
+
+httpd::ssl_listen 0.0.0.0 443
